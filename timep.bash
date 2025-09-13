659822b668821a46ee33fdeea583928e1dfeb50d
Anthony <107448833+jkool702@users.noreply.github.com>
2025-09-13 01:35:27 -0400

    Update timep.bash

--- timep.bash
+++ timep.bash
@@ -596,7 +596,7 @@ fi
             timep_CMD_TYPE="BACKGROUND FORK"
         elif ${timep_IS_SUBSHELL_FLAG}; then
             timep_CMD_TYPE="SUBSHELL"
-            ${timep_IS_FUNC_FLAG_1} && { timep_IS_FUNC_FLAG_1=false; [[ ${timep_LINENO_OFFSET[${timep_FNEST_CUR}]} ]] || (( timep_LINENO_OFFSET[${timep_FNEST_CUR}] = LINENO - 1 )); }
+            ${timep_IS_FUNC_FLAG_1} && { timep_IS_FUNC_FLAG_1=false; [[ ${timep_LINENO_OFFSET[${timep_FNEST_CUR}]} ]] || (( timep_LINENO_OFFSET[${timep_FNEST_CUR}] = LINENO + 9 )); }
         elif [[ "${timep_BASH_COMMAND_PREV[${timep_FNEST_CUR}]}" == " (F) "* ]]; then
             timep_CMD_TYPE="FUNCTION (P)"
             timep_BASH_COMMAND_PREV[${timep_FNEST_CUR}]="${timep_BASH_COMMAND_PREV[${timep_FNEST_CUR}]# (F) }"
