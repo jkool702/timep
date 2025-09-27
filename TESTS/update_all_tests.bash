8100f687c5dd7d6925a094ee46e3cdb22fcc9dd5
Anthony <107448833+jkool702@users.noreply.github.com>
2025-09-27 10:44:26 -0400

    re-organize TESTS directory

--- TESTS/update_all_tests.bash
+++ TESTS/update_all_tests.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-timep /mnt/ramdisk/timep/TESTS/timep.tests.bash 
-cp -r timep.profiles/* /mnt/ramdisk/timep/TESTS/OUTPUT/
+timep_basedir='/mnt/ramdisk'
 
-for nn in /mnt/ramdisk/timep/TESTS/claude-opus-4.1/bash_profiler_destroyer.sh /mnt/ramdisk/timep/TESTS/claude-sonnet-4/claude.test.bash /mnt/ramdisk/timep/TESTS/gemini-2.5/gemini-2.5.test.bash /mnt/ramdisk/timep/TESTS/gemini-2.5/new/test.bash /mnt/ramdisk/timep/TESTS/glm-4.5/brutal_profiler_test.bash /mnt/ramdisk/timep/TESTS/grok-4/grok.test.bash /mnt/ramdisk/timep/TESTS/perplexity/test.perplexity.bash; do 
-timep "$nn"
-\cp -r timep.profiles/* "${nn%\/*}/"
+for nn in "${timep_basedir}"/timep/TESTS/OTHER/timep.tests.bash  "${timep_basedir}"/timep/TESTS/claude-opus-4.1/bash_profiler_destroyer.sh "${timep_basedir}"/timep/TESTS/claude-sonnet-4/claude.test.bash "${timep_basedir}"/timep/TESTS/gemini-2.5/gemini-2.5.test.bash "${timep_basedir}"/timep/TESTS/gemini-2.5/new/test.bash "${timep_basedir}"/timep/TESTS/glm-4.5/brutal_profiler_test.bash "${timep_basedir}"/timep/TESTS/grok-4/grok.test.bash "${timep_basedir}"/timep/TESTS/perplexity/test.perplexity.bash; do 
+    timep "$nn"
+    \cp -r ./timep.profiles/* "${nn%\/*}/"
 done
 
+\rm ./timep.profiles
 
