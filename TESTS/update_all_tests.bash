b3cb867646cc525f6e1468ca4ca25e7e9faf69c6
Anthony <107448833+jkool702@users.noreply.github.com>
2025-09-27 10:47:36 -0400

    update "update_all_tests.bash" script

--- TESTS/update_all_tests.bash
+++ TESTS/update_all_tests.bash
@@ -2,9 +2,9 @@
 
 timep_basedir='/mnt/ramdisk'
 
-for nn in "${timep_basedir}"/timep/TESTS/OTHER/timep.tests.bash  "${timep_basedir}"/timep/TESTS/claude-opus-4.1/bash_profiler_destroyer.sh "${timep_basedir}"/timep/TESTS/claude-sonnet-4/claude.test.bash "${timep_basedir}"/timep/TESTS/gemini-2.5/gemini-2.5.test.bash "${timep_basedir}"/timep/TESTS/gemini-2.5/new/test.bash "${timep_basedir}"/timep/TESTS/glm-4.5/brutal_profiler_test.bash "${timep_basedir}"/timep/TESTS/grok-4/grok.test.bash "${timep_basedir}"/timep/TESTS/perplexity/test.perplexity.bash; do 
-    timep "$nn"
-    \cp -r ./timep.profiles/* "${nn%\/*}/"
+for nn in TESTS/OTHER/timep.tests.bash  TESTS/claude-opus-4.1/bash_profiler_destroyer.sh TESTS/claude-sonnet-4/claude.test.bash TESTS/gemini-2.5/gemini-2.5.test.bash TESTS/gemini-2.5/new/test.bash TESTS/glm-4.5/brutal_profiler_test.bash TESTS/grok-4/grok.test.bash TESTS/perplexity/test.perplexity.bash; do 
+    timep "${timep_basedir}"/timep/${nn}"
+    \cp -r ./timep.profiles/* "${timep_basedir}"/timep/${nn%\/*}/"
 done
 
 \rm ./timep.profiles
